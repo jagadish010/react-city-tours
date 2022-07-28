79a7c081613c51bab522aeb55830346b34b1102a
Jagadish <jagadishvp010@gmail.com>
2022-07-28 19:32:19 +0530

    first commit

---
+++ src/components/tourData.js
@@ -0,0 +1,34 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "new york",
+    img: "./img/newyork.jpeg",
+    name: "new york bridge tour",
+    info:
+      "Tour the whole New-york city with the best travel guide as your guest!"
+  },
+  {
+    id: 2,
+    city: "paris",
+    img: "./img/paris.jpeg",
+    name: "paris lights tour",
+    info:
+      "Explore the whole new Paris and relive some best moments with your family!"
+  },
+  {
+    id: 3,
+    city: "london",
+    img: "./img/london.jpeg",
+    name: "london royal palace tour",
+    info:
+      "Travel and stay at the best Palace present in the UK and explore more!"
+  },
+  {
+    id: 4,
+    city: "tokyo",
+    img: "./img/tokyo.jpeg",
+    name: "tokyo sushi tour",
+    info:
+      "Plan your trip to Tokyo this vacation and spend some freetime with your friends!"
+  }
+];
